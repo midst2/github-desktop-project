73462c5f4e31e93a834a4afb5c606a7126712cb3
midst2 <work.amiryunuh@gmail.com>
2025-12-18 11:41:18 +0700

    table

---
+++ src/Table.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Table = () => {
+  return (
+    <>
+      <table border="1">
+        <th>Product</th>
+        <th>Price</th>
+
+        <tr>
+          <td>React</td>
+          <td>320</td>
+        </tr>
+
+        <tr>
+          <td>React Native</td>
+          <td>340</td>
+        </tr>
+      </table>
+    </>
+  );
+};
