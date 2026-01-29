926b9174aeb18a7aacac1deca2613d6562d72cbf
midst2 <work.amiryunuh@gmail.com>
2026-01-29 19:23:39 +0700

    context with state (class comp, func comp)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import  {userContext} from "./context/context";
-import Content from "./context/context-content";
+import Header2 from "./context/context-header2";
+import Content2 from "./context/context-content2";
+import "./style.css"
 
 function App() {
+  let [user, setUser] = React.useState()
+
+  let a = ""
+  let b = ""
+  console.log(a || b)
   return <>
-    <userContext.Provider value = {"Tom Jerry"}>
-    <Content/>
+    <userContext.Provider value ={[user, setUser]}>
+    <Header2/>
+    <Content2/>
     </userContext.Provider>
       
     
