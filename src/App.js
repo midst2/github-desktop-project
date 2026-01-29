9fd2c9ff487912e12b8df56f16ccb836b56a3883
midst2 <work.amiryunuh@gmail.com>
2026-01-29 18:25:51 +0700

    use context (first time)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import StateFunc from "./state-func";
+import  {userContext} from "./context/context";
+import Content from "./context/context-content";
 
 function App() {
   return <>
-    <StateFunc></StateFunc>
+    <userContext.Provider value = {"Tom Jerry"}>
+    <Content/>
+    </userContext.Provider>
+      
+    
   </>
 }
 
